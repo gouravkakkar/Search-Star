10cab96b270d2cabbb5e733ea6199efdf0e79763
Gourav Kakkar <gouravkakkar@gmail.com>
2013-07-02 19:03:00 +0530

    Search Result Categorized
    Snippet of search result Added.
    Result categorized on the bases of files, where hits were found.

--- src/com/searchStar/search/Search.java
+++ src/com/searchStar/search/Search.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.regex.Pattern;
@@ -21,23 +23,37 @@ public class Search {
 	
 	static FileWriter fw;
 	
+	public static void main(String args[]) throws IOException{
+		String[] extToSearch={".xqy"};
+		Search.search("C:\\Program Files\\MarkLogic\\Modules\\Xplana2", "C:\\Users\\vhq\\Desktop", "bookSetting", extToSearch);
+	}
+	
 	public static void search(String inputFolder,String outputFolderPath,String keyword,String[] extToSearch) throws IOException{
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 			
-		File outputFolder =new File(outputFolderPath+"//"+timeStamp+".txt");
+		File outputFolder =new File(outputFolderPath+"//"+keyword+"-"+timeStamp+".txt");
 		//if file doesnt exists, then create it
 		if(!outputFolder.exists()){
 			outputFolder.createNewFile();
 		}
 		
 		fw=new FileWriter(outputFolder);
-		listFilesForFolder(new File(inputFolder),keyword,extToSearch);
+		long start = System.currentTimeMillis();
+		int resultCount=listFilesForFolder(new File(inputFolder),keyword,extToSearch);
+		long end = System.currentTimeMillis();
+		NumberFormat formatter = new DecimalFormat("#0.00000");
+		System.out.print(resultCount+" results in " + formatter.format((end - start) / 1000d) + " seconds");
 		fw.close();
 	}
 
 
 
-	private static void listFilesForFolder(File folder, String keyWord, String[] extToSearch) throws IOException {
+	private static int listFilesForFolder(File folder, String keyWord, String[] extToSearch) throws IOException {
+		
+		int totalCount=0;
+		String resultBuffer="";
+		
+		
 	    for (File fileEntry : folder.listFiles()) {
 	    	String fileName=fileEntry.getName();
 	    	
@@ -53,17 +69,29 @@ public class Search {
 	            		try{
 	            			BufferedReader input = new BufferedReader(new FileReader(fileEntry));
 	            			String line="";
+	            			boolean found=false;
+	            			String fileScopedResult="";
 	            			
 	            			while (line != null)
 	            			{
 	            			    line = input.readLine(); 
 	            			    if(line!=null){
 	            			    	if(Pattern.compile(Pattern.quote(keyWord), Pattern.CASE_INSENSITIVE).matcher(line).find()){
-	            			    		fw.write("Found at location="+fileEntry.getAbsolutePath()+"\n");
+	            			    		found=true;
+	            			    		totalCount++;
+	            			    		fileScopedResult=fileScopedResult+line+"\n";
+	            			    		//fw.write("Found at location="+fileEntry.getAbsolutePath()+"\n");
+	            			    		//fw.write(line+"\n");
 		            				}	
 	            			    }
 	            				
 	            			}
+	            			
+	            			if(found){
+	            				fileScopedResult="FOUND AT LOCATION="+fileEntry.getAbsolutePath()+"\n"+fileScopedResult+"\n";
+	            				resultBuffer=resultBuffer+fileScopedResult;
+	            			}
+	            			
 	            			}catch(Exception e){
 	            				System.out.println("In exception"+e);
 	            				fw.close();
@@ -72,6 +100,8 @@ public class Search {
 	            }
 	            }
 	    }
+	    fw.write(resultBuffer);
+	    return totalCount;
 	}
 	
 	
